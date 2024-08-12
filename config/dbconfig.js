01c9d957e35d776c6da85e496b539f4ef2ae0487
Rashid <siddiqui20042007@gmail.com>
2024-08-12 21:24:58 +0530

    removed unneccesary code

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -14,16 +14,6 @@ const dbDetails = {
     }
 }
 
-// const connectionstring = "mysql://3ggajbNmhPba8bC.root:yavfgASvpEyhO4wA@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Cluster0?sslaccept=strict";
-
-// const dbDetails = {
-//     host: 'localhost:3307',
-//     user: 'root',
-//     password: 'root',
-//     database: 'tests',
-//     dialect: 'mysql',
-// }
-
 let connection = mysql.createConnection(dbDetails);
 
 module.exports = { dbDetails, connection }; 
